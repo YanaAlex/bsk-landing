fd3c996fc4ed5f459e20044de1bb2d44d42fb69b
Yana <Aleksandrovayana040793@gmail.com>
2019-04-17 23:02:06 +0700

    Add slider of range + slider of tables

---
+++ script.js
@@ -0,0 +1,29 @@
+var owl0 = $('.slider0');
+owl0.owlCarousel({
+    items: 1,
+    loop: true,
+    nav: false,
+    dots: false,
+});
+
+$('.custom-owl-next-button0').click(function() {
+    owl0.trigger('next.owl.carousel');
+})
+$('.custom-owl-prev-button0').click(function() {
+    owl0.trigger('prev.owl.carousel');
+})
+
+var owl1 = $('.slider1');
+owl1.owlCarousel({
+    items: 1,
+    loop: true,
+    nav: false,
+    dots: false,
+});
+
+$('.custom-owl-next-button1').click(function() {
+    owl1.trigger('next.owl.carousel');
+})
+$('.custom-owl-prev-button1').click(function() {
+    owl1.trigger('prev.owl.carousel');
+})
